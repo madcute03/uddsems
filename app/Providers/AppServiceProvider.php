0249f08cecee1a00b353848594c8e1400f674a4d
madcute03 <jiannuel@example.com>
2025-09-18 16:29:44 +0800

    appservice fixed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
         Vite::prefetch(concurrency: 3);
         Inertia::share([
         'flash' => function () {
