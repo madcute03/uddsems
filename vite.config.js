bbc7aad23c46bd16d866d666d53ab5f9d4086479
madcute03 <jiannuel@example.com>
2025-09-19 12:22:24 +0800

    g

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import * as terser from 'terser';
 
 export default defineConfig(({ command, mode }) => {
     // Load environment variables based on the current mode
@@ -41,6 +42,11 @@ export default defineConfig(({ command, mode }) => {
             manifest: true,
             sourcemap: isProduction ? false : 'inline',
             minify: isProduction ? 'terser' : false,
+            terserOptions: {
+                compress: {
+                    drop_console: true,
+                },
+            },
             chunkSizeWarningLimit: 1600,
             rollupOptions: {
                 input: 'resources/js/app.jsx',
